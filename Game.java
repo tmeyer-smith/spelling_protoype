d5612fd87ffba4fecaa2e643c5868175fd29c0ab
major-faber <tmey857@aucklanduni.ac.nz>
2016-09-16 14:30:54 +1200

    Add files via upload

--- Game.java
+++ Game.java
@@ -1,21 +1,23 @@
-
 public class Game {
 
     private static SpellingFrame _frame;
-
-
-    public static void main(String[] args) {
-        setUpFrame();
-        WordListsManager.initialise();
-        GameStateManager.initialiseGSM();
-    }
 
     private static void setUpFrame() {
         _frame = new SpellingFrame();
     }
 
-    public static void addToFrame(GameStatePanel gs) {
-        _frame.setContentPane(gs);
+    public static void main(String[] args) {
+        setUpFrame();
+        GameStateManager.initializeGSM();
+        _frame.setVisible(true);
     }
+
+    public static void addToFrame(GameStatePanel gs) {
+    	_frame.setContentPane(gs);
+    	_frame.repaint();
+    	_frame.revalidate();
+    }
+
+
 
 }
